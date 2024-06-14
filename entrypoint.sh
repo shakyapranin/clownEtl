250e2929541797e97732b885cd47a66f1c09d12a
praneen <praneenshakya@lftechnology.com>
2024-06-14 17:12:06 +0545

    Migrate docker compose changes into a docker file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+# Initialize the database, create user, run scheduler and run webserver
+# airflow db init &&
+# airflow users create --username admin --firstname Admin --lastname User --role Admin --email admin@example.com --password admin &&
+# airflow scheduler & airflow webserver
+
+
+
+#!/usr/bin/env bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
 # Initialize the database
-airflow db init
+# airflow db init
 
-# Create admin user (if not exists)
-airflow users create --username admin --firstname Admin --lastname User --role Admin --email admin@example.com --password admin || true
+# Create an admin user
+# airflow users create --username admin --firstname Admin --lastname User --role Admin --email admin@example.com --password admin
 
-# Start scheduler and webserver
-exec airflow scheduler &
-exec airflow webserver
+# Start the scheduler in the background
+# airflow scheduler
+
+# Start the webserver in the foreground (blocking call)
+# exec airflow webserver
